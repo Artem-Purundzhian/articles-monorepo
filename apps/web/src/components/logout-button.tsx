888f68753d78e2dd0150ec97d159b88607fdb983
artem145 <artempa1607@gmail.com>
2024-02-25 21:34:16 +0100

    file formatted

--- apps/web/src/components/logout-button.tsx
+++ apps/web/src/components/logout-button.tsx
@@ -1,21 +1,22 @@
-'use client';
-import { useCookies } from 'react-cookie';
-import { Button } from './ui/button'
-import { useRouter } from 'next/navigation';
+"use client";
+import { useCookies } from "react-cookie";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 function LogoutButton() {
-  const [, , removeCookie] = useCookies(['access_token']);
-  const router = useRouter()
-
+  const [, , removeCookie] = useCookies(["access_token"]);
+  const router = useRouter();
 
   function logout() {
-    removeCookie('access_token')
-    router.refresh()
+    removeCookie("access_token");
+    router.refresh();
   }
 
-  return <Button variant='secondary' size='sm' onClick={logout}>
-    Log out
-  </Button>
+  return (
+    <Button variant="secondary" size="sm" onClick={logout}>
+      Log out
+    </Button>
+  );
 }
 
 export default LogoutButton;
