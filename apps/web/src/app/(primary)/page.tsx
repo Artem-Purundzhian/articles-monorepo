a74165ed861efd0395cb7ef0e1ddb61ff51ebfc3
artem145 <artempa1607@gmail.com>
2024-03-01 23:34:30 +0100

    add search params to homepage

--- apps/web/src/app/(primary)/page.tsx
+++ apps/web/src/app/(primary)/page.tsx
@@ -1,6 +1,18 @@
-import ArticlesList from '@/components/articles-list';
+import ArticlesList from "@/components/articles-list";
 
-export default function Home() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  console.log(query, currentPage);
+
   return (
     <main className="mx-auto max-w-2xl container px-4">
       <ArticlesList />
